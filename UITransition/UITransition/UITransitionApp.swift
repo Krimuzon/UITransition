b5e09f74b063226833c718af2774b767c092592e
Krimuzon <cristianamoddio@gmail.com>
2022-03-23 11:36:46 +0100

    First

---
+++ UITransition/UITransition/UITransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITransitionApp.swift
+//  UITransition
+//
+//  Created by Cristian Amoddio on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct UITransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
