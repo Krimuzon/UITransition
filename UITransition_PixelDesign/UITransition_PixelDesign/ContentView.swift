2a5afb2eabfcb24e52a19720d139710ce0b0a6df
enni <annaizzo7@hotmail.it>
2022-03-25 08:57:14 +0100

    Extra_Pixel Design

---
+++ UITransition_PixelDesign/UITransition_PixelDesign/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  UITransition_PixelDesign
+//
+//  Created by Anna Izzo on 25/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{
+            Color(UIColor.red)
+            
+            VStack{
+                Text("Hello, world!")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Image(systemName: "arrow.right.circle.fill")
+                    .font(.system(size: 100, weight: .medium, design: .default))
+                    .overlay {
+                        UIKitButtonRepresentable()
+                    }
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITransition_PixelDesign/UITransition_PixelDesign/UITransition_PixelDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITransition_PixelDesignApp.swift
+//  UITransition_PixelDesign
+//
+//  Created by Anna Izzo on 25/03/22.
+//
+
+import SwiftUI
+
+@main
+struct UITransition_PixelDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
